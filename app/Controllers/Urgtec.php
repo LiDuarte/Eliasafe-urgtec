6bda5932f6ad7785a3354ffe1841d120e8b5bbb1
Eliasafe D. Pinho <li_okay@hotmail.com>
2022-03-02 21:13:31 -0300

    configurações iniciais framework codeigniter/bootstrap

--- app/Controllers/Urgtec.php
+++ app/Controllers/Urgtec.php
@@ -2,11 +2,11 @@
 
 namespace App\Controllers;
 
-class Home extends BaseController
+class Urgtec extends BaseController
 {
     public function index()
     {
         
-        return view('welcome_message');
+        // return view('welcome_message');
     }
 }
